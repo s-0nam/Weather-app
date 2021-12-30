42e3435e09d5afdf3560307cc41a39c989838051
sonam <sonamkhurana71098@gmail.com>
2021-12-30 21:06:09 +0530

    updated weather info

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,10 +9,11 @@ const forecast = (latitude, longitude, callback) => {
          } else if (body.error) {
             callback('Invalid input', undefined)
          } else {
+             console.log(body)
             const temperature =  body.current.temperature
             const feelslike =  body.current.feelslike
             const weather_description =  body.current.weather_descriptions[0]
-            callback(undefined, weather_description + '. It is currently '+ temperature + ' degrees out. It feels like ' + feelslike + ' degrees out')
+            callback(undefined, weather_description + '. It is currently '+ temperature + ' degrees out. It feels like ' + feelslike + ' degrees out. The wind speed is ' + body.current.wind_speed)
         }
     })
 }
